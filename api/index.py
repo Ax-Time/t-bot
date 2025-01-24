86b81330949a196ea9551022b73d4f3031b9a8e5
AxTime <giacomo.cartechini@mail.polimi.it>
2025-01-24 15:59:58 +0100

    wip

--- api/index.py
+++ api/index.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from typing import Optional
+import telegram
 
+load_dotenv()
+TOKEN = os.environ.get('TOKEN')
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"message": "Hello World"}
+
+@app.post('/webhook')
+def webhook(webhook_data):
+    print(webhook_data)
