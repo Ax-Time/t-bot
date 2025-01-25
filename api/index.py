c8b65e1c3b2d60ac928506721376ebd7bb2c66fb
AxTime <giacomo.cartechini@mail.polimi.it>
2025-01-25 11:21:15 +0100

    wip

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from telegram import Update, Bot
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 app = FastAPI()
+bot = Bot(os.environ.get("TOKEN"))
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/updates")
+async def handle_update(update: dict):
+    update = Update.de_json(update, bot)
+    return {"update": update}
